7c9b78912262e132eacd5ad4aeac9349d3aa01d7
SandroMaglione <lass.maglio@gmail.com>
2023-11-15 05:46:11 +0100

    next request in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { Effect, pipe } from "effect";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
   matcher: ["/((?!_next|api/auth).*)(.+)"],
 };
 
-export async function middleware() {
+export async function middleware(request: NextRequest) {
   return pipe(
     Effect.promise(async () => NextResponse.next()),
     Effect.runPromise
